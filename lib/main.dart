c0c89bbe96bc0ab19fd7506cae808591029fc65e
Sergey-Engineer <disser1611@gmail.com>
2023-05-01 19:04:24 +0300

    Products Grid and Item Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/products_overview_screen.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,23 +13,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: ProductsOverviewScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('MyShop'),
-      ),
-      body: const Center(
-        child: Text('Let\'s build a shop!'),
-      ),
-    );
-  }
-}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem(this.id, this.title, this.imageUrl, {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: const IconButton(onPressed: null, icon: Icon(Icons.favorite)),
+        trailing:
+            const IconButton(onPressed: null, icon: Icon(Icons.shopping_cart)),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+}
