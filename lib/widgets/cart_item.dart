4f789f8c1c0a475801bb5e4b0c6c1f3a1fa289ba
Sergey-Engineer <disser1611@gmail.com>
2023-05-05 11:02:09 +0300

    Remove Cart Item

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -4,29 +4,48 @@ import 'package:shop_app/providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  const CartItem(this.id, this.price, this.quantity, this.title, {Key? key})
+  const CartItem(this.id, this.productId, this.price, this.quantity, this.title,
+      {Key? key})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final cartItem = Provider.of<Cart>(context);
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: Padding(
-                padding: const EdgeInsets.all(5),
-                child: FittedBox(child: Text('\$$price'))),
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        color: Theme.of(context).colorScheme.error,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: FittedBox(child: Text('\$$price'))),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
